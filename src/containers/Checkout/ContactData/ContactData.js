fc120f521ca839b453c96a7c8307f22d60f3572a
Sebastian Carrera <sebascarreram@hotmail.com>
2020-01-17 10:02:36 -0500

    Modified for checkValidity

--- src/containers/Checkout/ContactData/ContactData.js
+++ src/containers/Checkout/ContactData/ContactData.js
@@ -1,5 +1,6 @@
 import React, { Component } from "react";
 import { connect } from "react-redux";
+import { updateObject, checkValidity } from "../../../shared/utility";
 
 import Button from "../../../components/UI/Button/Button";
 import Spinner from "../../../components/UI/Spinner/Spinner";
@@ -102,8 +103,8 @@ class ContactData extends Component {
 
   orderHandler = event => {
     event.preventDefault();
-    console.log(this.props.ingredients);
-    console.log("orderHandler");
+    // console.log(this.props.ingredients);
+    // console.log("orderHandler");
 
     const formData = {};
     for (let formElementIdentifier in this.state.orderForm) {
@@ -116,69 +117,30 @@ class ContactData extends Component {
       price: this.props.price,
       orderData: formData,
       userId: this.props.userId
-      ////////////////////////////////
     };
     this.props.onOrderBurger(order, this.props.token);
   };
-
-  ////////////////////////////////////////////////////////////////
-  ////////////////////////////////////////////////////////////////
-
-  checkValidity(value, rules) {
-    let isValid = true;
-
-    if (!rules) {
-      return true;
-    }
-
-    if (rules.required) {
-      isValid = value.trim() !== "" && isValid;
-    }
-
-    if (rules.minLength) {
-      isValid = value.length >= rules.minLength && isValid;
-    }
-
-    if (rules.isEMail) {
-      const pattern = /[a-z0-9!#$%&'*+/=?^_`{|}~-]+(?:\.[a-z0-9!#$%&'*+/=?^_`{|}~-]+)*@(?:[a-z0-9](?:[a-z0-9-]*[a-z0-9])?\.)+[a-z0-9](?:[a-z0-9-]*[a-z0-9])?/;
-      isValid = pattern.test(value) && isValid;
-    }
-
-    // if (rules.isNumeric) {
-    //   const pattern = "/^d+$/";
-    //   isValid = pattern.test(value) & isValid;
-    // }
-
-    if (rules.maxLength) {
-      isValid = value.length <= rules.maxLength && isValid;
-    }
-
-    return isValid;
-  }
-  ////////////////////////////////////////////////////////////////
-  ////////////////////////////////////////////////////////////////
+  ///////////////////////////////////
 
   inputChangedHandler = (event, inputIdentifier) => {
-    const updatedOrderForm = {
-      ...this.state.orderForm
-    };
     // console.log(updatedOrderForm);
 
-    const updatedFormElement = {
-      ...updatedOrderForm[inputIdentifier]
-    };
-    // console.log(updatedFormElement);
-    updatedFormElement.value = event.target.value;
-    // Valid or no
-    updatedFormElement.valid = this.checkValidity(
-      updatedFormElement.value,
-      updatedFormElement.validation
+    const updatedFormElement = updateObject(
+      this.state.orderForm[inputIdentifier],
+      {
+        value: event.target.value,
+        valid: checkValidity(
+          event.target.value,
+          this.state.orderForm[inputIdentifier].validation
+        ),
+        touched: true
+      }
     );
-    // Touch or no
-    updatedFormElement.touched = true;
-    //
-    updatedOrderForm[inputIdentifier] = updatedFormElement;
-    console.log(updatedFormElement);
+
+    const updatedOrderForm = updateObject(this.state.orderForm, {
+      [inputIdentifier]: updatedFormElement
+    });
+    // console.log(updatedFormElement);
 
     let formIsValid = true;
     for (let inputIdentifier in updatedOrderForm) {
