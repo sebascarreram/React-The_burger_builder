d7d5b5c2217d3f3b28b07db99d37c15f0448aa3c
Sebastian Carrera <sebascarreram@hotmail.com>
2020-01-18 06:23:11 -0500

    Updates

---
+++ precache-manifest.4cdbd25500660a9f220a8dbcf5770926.js
@@ -0,0 +1,54 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "f25fd3028aea3fda8c3f75a85fb1ae93",
+    "url": "/React-The_burger_builder/index.html"
+  },
+  {
+    "revision": "b510ec5acd3b7986be7c",
+    "url": "/React-The_burger_builder/static/css/3.09b8f97f.chunk.css"
+  },
+  {
+    "revision": "976c62ce415416d50657",
+    "url": "/React-The_burger_builder/static/css/4.a1aae4aa.chunk.css"
+  },
+  {
+    "revision": "cb3d1984f9b612a68e86",
+    "url": "/React-The_burger_builder/static/css/5.6af0d7c9.chunk.css"
+  },
+  {
+    "revision": "38efc2352a5ba435d74a",
+    "url": "/React-The_burger_builder/static/css/main.d7855cf9.chunk.css"
+  },
+  {
+    "revision": "4765575b0ee7de06934c",
+    "url": "/React-The_burger_builder/static/js/2.534bc738.chunk.js"
+  },
+  {
+    "revision": "84a2a7a390df1ac99fa4fadf23155e60",
+    "url": "/React-The_burger_builder/static/js/2.534bc738.chunk.js.LICENSE"
+  },
+  {
+    "revision": "b510ec5acd3b7986be7c",
+    "url": "/React-The_burger_builder/static/js/3.2e69869c.chunk.js"
+  },
+  {
+    "revision": "976c62ce415416d50657",
+    "url": "/React-The_burger_builder/static/js/4.577e6bfc.chunk.js"
+  },
+  {
+    "revision": "cb3d1984f9b612a68e86",
+    "url": "/React-The_burger_builder/static/js/5.8e7ad07e.chunk.js"
+  },
+  {
+    "revision": "38efc2352a5ba435d74a",
+    "url": "/React-The_burger_builder/static/js/main.efeec244.chunk.js"
+  },
+  {
+    "revision": "f8c1e2d63b7445ab77b8",
+    "url": "/React-The_burger_builder/static/js/runtime-main.b9884283.js"
+  },
+  {
+    "revision": "b8503d262bffbfb7c67fd6762963e7d1",
+    "url": "/React-The_burger_builder/static/media/burger-logo.b8503d26.png"
+  }
+]);
